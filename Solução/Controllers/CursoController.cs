2169b6ef3c479cb8b9d5d2f07a1dc0d624d0e983
natalia-2023-20 <nataliamadeira11@hotmail.com>
2024-04-26 16:31:35 -0300

    Add project files.

---
+++ Solução/Controllers/CursoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Solução.Controllers
+{
+    public class CursoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ Solução/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Solução.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
